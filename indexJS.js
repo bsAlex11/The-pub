f8f57bb0e4770c16382ddd7afd529848fe938efc
bsAlex11 <bsAlex11@users.noreply.github.com>
2017-03-29 14:44:38 +0300

    Delete indexJS.js

--- indexJS.js
+++
@@ -1,8 +0,0 @@
-var buton = document.getElementById("hamb-menu");
-
-   buton.addEventListener("click",apasa);
-
-   function apasa(){
-    var list = document.getElementsByTagName("ul")[0];
-     list.classList.toggle("open-on-click");
-   } 
